3df6ae8e9a1b0fd3f66b77af184fb0764d127643
Gang125 <46886103+Gang125@users.noreply.github.com>
2019-01-30 11:53:42 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,7 +1,7 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-var prefix = "$"
-var adminprefix = '$'
+var prefix = "D"
+var adminprefix = 'D'
 
 
 //bc
@@ -23,7 +23,7 @@ client.on("message", message => {
 //bc online
 
 
-  var prefix = "$";
+  var prefix = "D";
 
   client.on("message", message => {
   
